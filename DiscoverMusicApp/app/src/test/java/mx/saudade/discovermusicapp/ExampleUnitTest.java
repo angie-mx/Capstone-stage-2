7e69cbe8c01aad791ac6c20ed98d9e6c02215c92
Angie <angie.saudade@gmail.com>
2016-06-19 17:53:38 -0700

    feat: remove unused imports

--- DiscoverMusicApp/app/src/test/java/mx/saudade/discovermusicapp/ExampleUnitTest.java
+++ DiscoverMusicApp/app/src/test/java/mx/saudade/discovermusicapp/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package mx.saudade.discovermusicapp;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
