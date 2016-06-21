de1ec0815a4c1fc7b21db9fd1c8835f68ffabf57
Angelica Mendez <angie.saudade@gmail.com>
2016-06-21 13:35:06 -0700

    feat: permission handler added to location handler

--- DiscoverMusicApp/app/src/main/java/mx/saudade/discovermusicapp/handlers/LocationHandler.java
+++ DiscoverMusicApp/app/src/main/java/mx/saudade/discovermusicapp/handlers/LocationHandler.java
@@ -1,15 +1,13 @@
 package mx.saudade.discovermusicapp.handlers;
 
-import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -35,8 +33,12 @@ public class LocationHandler implements GoogleApiClient.ConnectionCallbacks
 
     private Location location;
 
-    public LocationHandler(Context context) {
+    PermissionHandler permissionHandler;
+
+    public LocationHandler(Activity context) {
         this.context = context;
+        permissionHandler = new PermissionHandler(context
+                , PermissionHandler.Permission.LOCATION);
     }
 
     public void onCreate() {
@@ -57,19 +59,16 @@ public class LocationHandler implements GoogleApiClient.ConnectionCallbacks
         googleApiClient.disconnect();
     }
 
+    public void onRequestPermissionsResult(int requestCode, int[] grantResults) {
+        if (permissionHandler.checkRequestPermissionResult(requestCode, grantResults)) {
+            Log.v(TAG, "onRequestPermissionsResult permission granted");
+            googleApiClient.reconnect();
+        }
+    }
+
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        //FIXME change this implementation
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context
-                , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (!permissionHandler.checkPermission()) {
             return;
         }
         location = LocationServices.FusedLocationApi.getLastLocation(
