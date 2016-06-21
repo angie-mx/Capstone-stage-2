17cb63b561b2796b05cf57c23471e0d1c7806136
Angelica Mendez <angie.saudade@gmail.com>
2016-06-21 14:34:25 -0700

    feat: google analytics added

---
+++ DiscoverMusicApp/app/src/main/java/mx/saudade/discovermusicapp/AnalyticsApplication.java
@@ -0,0 +1,37 @@
+package mx.saudade.discovermusicapp;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
+/**
+ * @author Angelica Mendez, on 6/21/16.
+ */
+public class AnalyticsApplication extends Application {
+
+    private Tracker mTracker;
+
+    synchronized private Tracker getDefaultTracker() {
+        if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            mTracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return mTracker;
+    }
+
+    public void trackScreen(String screen) {
+        getDefaultTracker().setScreenName(screen);
+        getDefaultTracker().send(new HitBuilders.ScreenViewBuilder().build());
+    }
+
+    public void trackEvent(String category, String action, String label) {
+        getDefaultTracker().send(new HitBuilders.EventBuilder()
+                .setCategory(category)
+                .setAction(action)
+                .setLabel(label)
+                .build());
+    }
+}
