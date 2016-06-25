99ea7ab779ac65370053babc24d3d28ea7cdf600
Angie <angie.saudade@gmail.com>
2016-06-24 23:19:33 -0700

    feat: data base helper created

--- DiscoverMusicApp/app/src/main/java/mx/saudade/discovermusicapp/data/AppDataBaseHelper.java
+++ DiscoverMusicApp/app/src/main/java/mx/saudade/discovermusicapp/data/AppDataBaseHelper.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import mx.saudade.discovermusicapp.data.AppContract.ArtistEntry;
+import mx.saudade.discovermusicapp.data.AppContract.TrackEntry;
+
 /**
  * Created by angie on 6/23/16.
  */
@@ -19,11 +22,38 @@ public class AppDataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_ARTIST_TABLE =
+                "CREATE TABLE " + ArtistEntry.TABLE_NAME + " (" +
+                        ArtistEntry._ID + " INTEGER PRIMARY KEY, " +
+                        ArtistEntry.COLUMN_NAME + " VARCHAR(100), " +
+                        ArtistEntry.COLUMN_IMG_URL + " VARCHAR(255) " +
+                        ");";
 
+        final String SQL_CREATE_TRACK_TABLE =
+                "CREATE TABLE " + TrackEntry.TABLE_NAME + " (" +
+                        TrackEntry._ID + " INTEGER PRIMARY KEY, " +
+                        TrackEntry.COLUMN_TITLE + " VARCHAR(100), " +
+                        TrackEntry.COLUMN_ARTIST_ID + " INTEGER, " +
+                        TrackEntry.COLUMN_RELEASE_DATE + " INTEGER, " +
+                        TrackEntry.COLUMN_GENRE + " VARCHAR(100), " +
+                        TrackEntry.COLUMN_AROUSAL + " INTEGER, " +
+                        TrackEntry.COLUMN_VALENCE + " INTEGER, " +
+                        TrackEntry.COLUMN_LYRICS + " TEXT" +
+                        ");";
+
+        db.execSQL(SQL_CREATE_ARTIST_TABLE);
+        db.execSQL(SQL_CREATE_TRACK_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        final String SQL_DROP_MOVIE_ARTIST = "DROP TABLE IF EXISTS " + ArtistEntry.TABLE_NAME;
+        final String SQL_DROP_REVIEW_TRACK = "DROP TABLE IF EXISTS " + TrackEntry.TABLE_NAME;
 
+        db.execSQL(SQL_DROP_MOVIE_ARTIST);
+        db.execSQL(SQL_DROP_REVIEW_TRACK);
+
+        this.onCreate(db);
     }
+
 }
