410d6e96f1384e859d05138dd3cbd32e294a4a17
Angie <angie.saudade@gmail.com>
2016-07-03 00:42:02 -0700

    feat: main activity starts discover fragment

--- DiscoverMusicApp/app/src/main/java/mx/saudade/discovermusicapp/MainActivity.java
+++ DiscoverMusicApp/app/src/main/java/mx/saudade/discovermusicapp/MainActivity.java
@@ -1,13 +1,35 @@
 package mx.saudade.discovermusicapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import mx.saudade.discovermusicapp.fragments.DiscoverFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final String PLAYLIST_FRAGMENT_ID = "PLAYLIST_FRAGMENT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new DiscoverFragment());
     }
+
+    public void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.activity_fragment_container, fragment)
+                .commit();
+    }
+
+    public void loadFragment(Fragment fragment, String id) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.activity_fragment_container, fragment)
+                .addToBackStack(id)
+                .commit();
+    }
+
 }
